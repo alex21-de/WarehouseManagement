bedf005844c59afea7121b13ca6eee57dba249bd
Alexei Rotari <alex21.it.dd@gmail.com>
2026-09-16 15:08:16 +0200

    update Product form

---
+++ src/WarehouseManagement.Web/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace WarehouseManagement.Web.Models
+{
+	public class Category
+	{
+		public Guid Id { get; set; }
+		public string? Name { get; set; }
+		public string? Description { get; set; }
+		public ICollection<Product>? Products { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpdatedAt { get; set; }
+
+		public Category()
+		{
+			Products = new List<Product>();
+		}
+	}
+}
--- src/WarehouseManagement.Web/Models/Product.cs
+++ src/WarehouseManagement.Web/Models/Product.cs
@@ -5,8 +5,22 @@ namespace WarehouseManagement.Web.Models
 	public class Product
 	{
 		public Guid Id { get; set; }
+		public string? SKU { get; set; }
 		public string? Name { get; set; }
 		public string? Description { get; set; }
-		public string? Category { get; set; }
+		public decimal Price { get; set; }
+		public Guid CategoryId { get; set; }
+		public Category? Category { get; set; }
+		public bool IsActive { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpdatedAt { get; set; }
+
+		public Product()
+		{
+			Category = new Category();
+			CreatedAt = DateTime.Now;
+			UpdatedAt = DateTime.Now;
+		}
+
 	}
 }
